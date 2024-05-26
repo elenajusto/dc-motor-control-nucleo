e607c8207f4dc3ea6dc03a4d77df4e3419e117a4
Elena Justo <elenaj@protonmail.ch>
2024-05-26 18:36:58 +1000

    defined all register addresses except reserved ones into pressure sensor header file

--- Core/Inc/spl06-007.h
+++ Core/Inc/spl06-007.h
@@ -22,8 +22,57 @@
 /*
  * REGISTERS (p.17)
  */
-#define SPL06_REG_
 
+/* Air Pressure Registers */
+#define SPL06_REG_PSR_B2_ADDR	0x00	/* highest byte of the three bytes measured pressure value */
+#define SPL06_REG_PSR_B1_ADDR	0x01	/* middle byte of the three bytes measured pressure value */
+#define SPL06_REG_PSR_B0_ADDR	0x02	/* lowest byte of the three bytes measured pressure value */
+
+/* Temperature Registers */
+#define SPL06_REG_TMP_B2_ADDR	0x03	/* highest byte of the three bytes measured temperature value */
+#define SPL06_REG_TMP_B1_ADDR	0x04	/* middle byte of the three bytes measured temperature value */
+#define SPL06_REG_TMP_B0_ADDR	0x05	/* lowest part of the three bytes measured temperature value */
+
+/* Pressure Configuration Register */
+#define SPL06_REG_PRS_CFG_ADDR	0x06
+
+/* Temperature Configuration Register */
+#define SPL06_REG_TMP_CFG_ADDR	0x07
+
+/* Sensor Operating Mode and Status Register */
+#define SPL06_REG_MEAS_CFG_ADDR	0x08
+
+/* Interrupt and FIFO Configuration Register */
+#define SPL06_REG_CFG_REG_ADDR	0x09
+
+/* Interrupt Status Register */
+#define SPL06_REG_INT_STS_ADDR	0x0A
+
+/* FIFO Status Register */
+#define SPL06_REG_FIFO_STS_ADDR	0x0B
+
+/* Soft Reset and FIFO Flush Register */
+#define SPL06_REG_RESET_ADDR	0x0C
+
+/* Calibration Coefficient Registers */
+#define SPL06_REG_C0			0x10
+#define	SPL06_REG_C01C1			0x11
+#define SPL06_REG_C1			0x12
+#define SPL06_REG_C00_1			0x13
+#define SPL06_REG_C00_2			0x14
+#define SPL06_REG_C00C10		0x15
+#define SPL06_REG_C10_1			0x16
+#define SPL06_REG_C10_2			0x17
+#define SPL06_REG_C01_1			0x18
+#define SPL06_REG_C01_2			0x19
+#define SPL06_REG_C11_1			0x1A
+#define SPL06_REG_C11_2			0x1B
+#define SPL06_REG_C20_1			0x1C
+#define SPL06_REG_C20_2			0x1D
+#define SPL06_REG_C21_1			0x1E
+#define SPL06_REG_C21_2			0x1F
+#define SPL06_REG_C30_1			0x20
+#define SPL06_REG_C30_2			0x21
 
 /*
  * SENSOR STRUCT
